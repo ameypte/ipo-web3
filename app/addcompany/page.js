339f491cadecc9f538fe840bdd3158a523f5fe9d
DurevshChaudhari <durveshchaudhari11@gmail.com>
2024-02-11 00:45:57 +0530

    navbar_and_other_GUI

--- app/addcompany/page.js
+++ app/addcompany/page.js
@@ -29,7 +29,7 @@ export default function IPOListingForm() {
                     type="text"
                     name="recipeName"
                     id="recipeName"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300  text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Enter company name"
                     required
                   />
--- app/components/addcompbtn.js
+++ app/components/addcompbtn.js
@@ -5,11 +5,40 @@ export default function addcompanybtn() {
     const router = useRouter();
     return (
         
+        <section class="bg-[url(/CTABg.svg)] w-full h-full bg-no-repeat bg-cover relative ctaSection min-h-[510px]">
+      <div class="w-11/12 max-w-[1080px] relative flex flex-row mx-auto items-center justify-between space-x-20 pt-16 ">
+
+        <div class="flex flex-col gap-5 mt-12">
+          <h2 class="font-mullish font-bold text-2xl text-white">
+            Take Your Company Public: Click Here to Launch Your IPO Journey
+          </h2>
+          <div class="w-6 h-1 bg-greenLight"></div>
+          
+          <button class="min-w-[32px] font-mullish text-sm font-bold bg-white text-lightBlue300 border flex rounded-sm items-center hover:text-lightBlue500 transition-all duration-200 py-3 px-4 place-self-start " onClick={() => {
+            router.push("/addcompany");}}>
+        Add Company IPO
+            <svg
+                viewBox="0 0 24 24"
+                focusable="false"
+                class="w-[14px] h-[14px] ml-3"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"
+                ></path>
+              </svg>
+          </button>
+        </div>
+
+        <div>
+          <img src="./ctaImg.svg" alt="" width="240px" height="282px" class="mt-16 pt-5"/>
+        </div>
+
+      </div>
+      
+    </section>
+    
         
-        <div class="p-8 flex justify-center">
-            
-        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-96" onClick={() => {
-            router.push("/addcompany");}}>Add company IPO
-                            </button></div>
+        
     )
 }
