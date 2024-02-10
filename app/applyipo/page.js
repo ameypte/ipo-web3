8d31fb0549fd66f7b93af408da1865452830e847
bhaveshxop <113847606+bhaveshxop@users.noreply.github.com>
2024-02-11 02:43:56 +0530

    some

--- app/applyipo/page.js
+++ app/applyipo/page.js
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import NavBar from "../components/NavBar";
 
 export default function IPOApplicationForm() {
   const companyName = "ABC Ltd."; // 1st input
@@ -24,75 +25,78 @@ export default function IPOApplicationForm() {
   };
 
   return (
-    <form className="max-w-sm mx-auto" onSubmit={handleApplyIPO}>
-      <div className="mb-5">
-        <label
-          htmlFor="companyName"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    <>
+      <NavBar />
+      <form className="max-w-sm mx-auto" onSubmit={handleApplyIPO}>
+        <div className="mb-5">
+          <label
+            htmlFor="companyName"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Company Name
+          </label>
+          <input
+            type="text"
+            id="companyName"
+            value={companyName}
+            readOnly
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="sharePrice"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Share Price
+          </label>
+          <input
+            type="text"
+            id="sharePrice"
+            value={`Rs ${fixedSharePrice}`} // Display fixed Share Price
+            readOnly
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="lotsApplied"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Lots Applied
+          </label>
+          <input
+            type="number"
+            id="lotsApplied"
+            value={lotsApplied}
+            onChange={handleLotsAppliedChange}
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            placeholder="Enter number of lots"
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="totalAmount"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Total Amount
+          </label>
+          <input
+            type="text"
+            id="totalAmount"
+            value={`Rs ${totalAmount}`}
+            readOnly
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Company Name
-        </label>
-        <input
-          type="text"
-          id="companyName"
-          value={companyName}
-          readOnly
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-        />
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="sharePrice"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Share Price
-        </label>
-        <input
-          type="text"
-          id="sharePrice"
-          value={`$${fixedSharePrice}`} // Display fixed Share Price
-          readOnly
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-        />
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="lotsApplied"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Lots Applied
-        </label>
-        <input
-          type="number"
-          id="lotsApplied"
-          value={lotsApplied}
-          onChange={handleLotsAppliedChange}
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="Enter number of lots"
-          required
-        />
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="totalAmount"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Total Amount
-        </label>
-        <input
-          type="text"
-          id="totalAmount"
-          value={`$${totalAmount}`}
-          readOnly
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-        />
-      </div>
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Apply IPO
-      </button>
-    </form>
+          Apply IPO
+        </button>
+      </form>
+    </>
   );
 }
